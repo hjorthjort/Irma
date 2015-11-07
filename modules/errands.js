c9c3e3fec2547e7aed358458bd08ae33b0351b24
Christian Persson <saser@live.se>
2015-11-07 04:07:37 +0100

    Minor refactoring

--- modules/errands.js
+++ modules/errands.js
@@ -1,10 +1,10 @@
 var exports = {};
 
-var dbCredentials = {
-    url: "https://490f0a5c-5c8d-435c-a709-5a661c8356f6-bluemix:aa85309d3a2d7b79526b89a1ad37b8f2d32a37d0205fcd0e19c091915993707b@490f0a5c-5c8d-435c-a709-5a661c8356f6-bluemix.cloudant.com"
-};
-
-var cloudant = require('cloudant')(dbCredentials.url);
+var cloudant = require('./cloudant').connect(function (err, cloudant) {
+    if (err) {
+        return console.log(err);
+    }
+});
 var errands = cloudant.db.use('errands');
 
 exports.add = function (params, callback) {
