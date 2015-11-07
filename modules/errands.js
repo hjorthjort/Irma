286d3b51c8fb5cea946f9cfa221f0d2539afa31d
Micro <maglars2@gmail.com>
2015-11-08 00:46:21 +0100

    Added username and age (if they exist) to an incoming errand

--- modules/errands.js
+++ modules/errands.js
@@ -1,28 +1,44 @@
 var exports = {};
 
 var fs = require('fs');
+var customers = require('./customers');
 
 var db = require('./db');
 var errands = db.use('errands');
 
 exports.add = function (pathToRecording, description, tags, phone_number, timestamp, callback) {
-    errands.insert({
-        description: description,
-        tags: tags,
-        phone_number: phone_number,
-        timestamp: timestamp,
-        completed: false
-    }, function (err, body) {
-        if (err) {
-            return callback(err);
-        }
+	customers.getByPhoneNumber(phone_number, function(err, result) {
+		if(err) {
+			return callback(err);
+		}
+		
+		name = '';
+		age = '';
+		if(result.length > 0) {
+			name = result[0].name;
+			age = result[0].age;
+		}
+		
+		errands.insert({
+			description: description,
+			tags: tags,
+			phone_number: phone_number,
+			timestamp: timestamp,
+			user_name: name,
+			user_age: age,
+			completed: false
+		}, function (err, body) {
+			if (err) {
+				return callback(err);
+			}
 
-        fs.createReadStream(pathToRecording).pipe(
-            errands.attachment.insert(body.id, 'recording.wav', null, 'audio/wav', { rev: body.rev })
-        ).on('end', function () {
-            callback(null, body);
-        });
-    });
+			fs.createReadStream(pathToRecording).pipe(
+				errands.attachment.insert(body.id, 'recording.wav', null, 'audio/wav', { rev: body.rev })
+			).on('end', function () {
+				callback(null, body);
+			});
+		});
+	});
 };
 
 exports.get = function (id, callback) {
