9cd92ed5a16893b09e9d7e2014677052d8be5d03
Mats Högberg <mats@hgbrg.se>
2015-11-07 15:59:59 +0200

    removed index route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.sendfile('index.html');
-});
+
 
 module.exports = router;
