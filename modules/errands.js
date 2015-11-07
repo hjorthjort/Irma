fc42c4b87928ad7842c4c5b920f19b9e80d1a446
Mats Högberg <mats@hgbrg.se>
2015-11-07 03:40:20 +0200

    get errands

--- modules/errands.js
+++ modules/errands.js
@@ -5,16 +5,25 @@ var dbCredentials = {
 };
 
 var cloudant = require('cloudant')(dbCredentials.url);
+var errands = cloudant.db.use('errands');
 
 exports.add = function (params, callback) {
-    var errands = cloudant.db.use('errands');
-
     errands.insert(params, function (err, body) {
         if (err) {
             return callback(err);
         }
 
         callback(null, body);
+    });
+};
+
+exports.getAll = function (callback) {
+    errands.list({include_docs: true}, function (err, result) {
+        if (err) {
+            callback(err);
+        }
+
+        callback(null, result);
     });
 };
 
--- routes/errands.js
+++ routes/errands.js
@@ -1,8 +1,17 @@
-var Cloudant = require('cloudant');
 var express = require('express');
 var router = express.Router();
 
 var errands = require('../modules/errands');
+
+router.get('/', function(req, res, next) {
+    errands.getAll(function (err, result) {
+        if (err) {
+            return console.log(err);
+        }
+
+        res.json(result);
+    });
+});
 
 router.post('/', function(req, res, next) {
     var recording = req.body.recording;
