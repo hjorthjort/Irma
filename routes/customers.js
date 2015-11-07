4eff4a65ed15d0985a96c3be99d7558ffec294ba
Alexander Hultnér <ahultner@gmail.com>
2015-11-07 23:52:10 +0200

    Can now post a user with following params 	customer_name 		_age 		_email 		_phone_number 		_klarna_token 		_password To customers in order to add a customer to the database. Klarna token isn't actually validated right now.

--- routes/customers.js
+++ routes/customers.js
@@ -2,6 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 var customers = require('../modules/customers');
+var bcrypt = require('bcrypt');
+var validator = require('validator');
+
+/**
+ * TODO: Implement actual validation against Klarna
+ * @param token
+ * @returns {boolean}
+ */
+function validateKlarnaToken(token){
+    return true;
+}
 
 router.get('/', function(req, res, next) {
     customers.getAll(function (err, result) {
@@ -13,15 +24,42 @@ router.get('/', function(req, res, next) {
     });
 });
 
+
 router.post('/', function(req, res, next) {
-    var name = req.body.name;
-    var age = req.body.age;
-    var phone_number = req.body.phone_number;
+    var name = req.body.customer_name;
+    var age = req.body.customer_age;
+    var phone_number = req.body.customer_phone_number;
+    var email = req.body.customer_email;
+    var klarna_token = req.body.customer_klarna_token;
+    var salt = bcrypt.genSaltSync();
+    var password = bcrypt.hashSync(req.body.customer_password, salt);
+
+    //TODO: Improve validation
+    if(
+        !validator.isEmail(email) ||
+        !validator.isLength(name, 1) ||
+        !validator.isInt(age) ||
+        !validator.isMobilePhone(phone_number) ||
+        !validateKlarnaToken(klarna_token)
+    ){
+        return res.status(500).json({
+            'error': 'Invalid input',
+            'email': validator.isEmail(email),
+            'name': validator.isLength(name, 1),
+            'age': validator.isInt(age),
+            'phone': validator.isMobilePhone(phone_number),
+            'klarna_token': validateKlarnaToken(klarna_token)
+        });
+    }
 
     var params = {
         name: name,
+        email: email,
         age: age,
-        phone_number: phone_number
+        klarna_token: klarna_token,
+        phone_number: phone_number,
+        salt: salt,
+        password: password
     };
 
     customers.add(params, function (err, result) {
