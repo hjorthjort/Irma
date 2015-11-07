04ecef81cc11c313a3af117457d59af4633f9c26
Christian Persson <saser@live.se>
2015-11-07 16:08:35 +0100

    get from errands

--- modules/errands.js
+++ modules/errands.js
@@ -24,6 +24,16 @@ exports.add = function (pathToRecording, description, tags, phone_number, timest
     });
 };
 
+exports.get = function (id, callback) {
+    errands.get(id, function (err, body) {
+        if (err) {
+            return callback(err);
+        }
+
+        callback(null, body);
+    });
+};
+
 exports.getAll = function (callback) {
     errands.list({include_docs: true}, function (err, result) {
         if (err) {
